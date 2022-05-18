d0abcecc4964ad381973b6c4c20042ef15b5611c
Vladas <146100@gmail.com>
2022-05-18 20:33:51 +0300

    new link

--- Casks/elastio.rb
+++ Casks/elastio.rb
@@ -2,7 +2,7 @@ cask "elastio" do
     version "0.15.0"
     sha256 :no_check
   
-    url "https://github.com/elastio/homebrew-tap/releases/download/Signed2/elastio.zip"
+    url "https://github.com/elastio/homebrew-tap/archive/refs/tags/Signed.zip"
     name "Elastio"
     desc "The Data Protection Stage of Your Pipeline"
     homepage "https://elastio.com/"
