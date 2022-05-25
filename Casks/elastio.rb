00e379074f488c88f5812e89bcb9e9d34b7d010b
Vladas Zakrevskis <146100@gmail.com>
2022-05-26 01:06:07 +0300

    sha

--- Casks/elastio.rb
+++ Casks/elastio.rb
@@ -1,6 +1,6 @@
 cask "elastio" do
     version "0.17.0"
-    sha256 :no_check
+    sha256 "971eab31bc8089d16a48de7684207b290211c968b495821e46161d99b429ae5b"
   
     url "https://s3.us-east-2.amazonaws.com/repo.assur.io/master/macos/aarch64/elastio.zip"
     name "Elastio"
