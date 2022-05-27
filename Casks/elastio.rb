85054d79beada2a444373aacaf774d92a073148d
Vladas Zakrevskis <146100@gmail.com>
2022-05-27 14:35:03 +0300

    release test

--- Casks/elastio.rb
+++ Casks/elastio.rb
@@ -1,23 +1,20 @@
 cask "elastio" do
-    version "0.17.0"
 
-    arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
-  
-    if Hardware::CPU.intel?
-      sha256 "08fbc19190b8b0ca2a69c2635053cf9d2cdc50795418e16832c20244e0f2b231"
-    else
-      sha256 "d2bda762e2ee317102e857218b1bdcb4a938c5651695c67dc8adbecea91057c3"
-    end
-  
-    url "https://s3.us-east-2.amazonaws.com/repo.assur.io/master/macos/#{arch}/elastio.zip"
-    name "Elastio"
-    desc "The Data Protection Stage of Your Pipeline"
-    homepage "https://elastio.com/"
-  
-    binary "elastio"
+  version "0.17.0"
 
-    # Cleanup example
-    # zap trash: [
-    #   "~/Library/Application Support/elastio",
-    # ]
+  arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
+
+  if Hardware::CPU.intel?
+    sha256 "2a99a9481f0ea049e74d43ddd6575ed89612f0d837f8309838e2e1173fe663c0"
+  else
+    sha256 "be147727e36a11a1b75979ba84c126b5f692bdff68d5301d3d2d669789727b1f"
   end
+
+  url "https://s3.us-east-2.amazonaws.com/repo.assur.io/release/macos/#{arch}/elastio.zip"
+  name "Elastio"
+  desc "The Data Protection Stage of Your Pipeline"
+  homepage "https://elastio.com/"
+
+  binary "elastio"
+
+end
