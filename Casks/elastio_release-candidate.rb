3dd2bbf8557f5d49b490255d1b1f1f8b84e2fbc1
Packaging Job <packaging@elastio.com>
2022-06-03 09:39:19 -0700

    release-candidate 7511

--- Casks/elastio_release-candidate.rb
+++ Casks/elastio_release-candidate.rb
@@ -1,13 +1,13 @@
 cask "elastio_release-candidate" do
 
-  version "0.16.0"
+  version "0.17.0"
 
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "6253aa5e0020dc06de2d77b4f513bd518792237381d5d18b3f22e3cfb5b14009"
+    sha256 "3db57e2528751057168c64fe3dd41fdf72bca520b8a21f5b775d2066d81f8eea"
   else
-    sha256 ""
+    sha256 "79e5aaaae18b72b0e0e5022c21f403412e2e57ece3319c6746a73f74c042f752"
   end
 
   url "https://s3.us-east-2.amazonaws.com/repo.assur.io/release-candidate/macos/#{arch}/elastio.zip"
@@ -17,8 +17,4 @@ cask "elastio_release-candidate" do
 
   binary "elastio"
 
-  # Cleanup example
-  # zap trash: [
-  #   "~/Library/Application Support/elastio",
-  # ]
 end
