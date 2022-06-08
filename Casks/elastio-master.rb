b0dbbb6be104669d775c4914a6274ea07e8ce1f9
Packaging Job <packaging@elastio.com>
2022-06-07 17:43:36 -0700

    master 7567

--- Casks/elastio-master.rb
+++ Casks/elastio-master.rb
@@ -5,9 +5,9 @@ cask "elastio-master" do
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "3f718161e5cb6ba9349bdf0745b149ce26a8c336da6a05ba0923dd6fa970b28a"
+    sha256 "9e161e1ee21d1caa19fa0bab4a7258349f2b0a1b8c76318b318020d5718e8eb5"
   else
-    sha256 "f7a631ce23ec6ddffc38074ea24ad9ac40c420adb2e4d45e1ae2ed76bfd14c1c"
+    sha256 "f80088a816cabf3b9dd06aa1c188a894845b8de946c67d1965f9775f11c22ffb"
   end
 
   url "https://s3.us-east-2.amazonaws.com/repo.assur.io/master/macos/#{arch}/elastio.zip"
