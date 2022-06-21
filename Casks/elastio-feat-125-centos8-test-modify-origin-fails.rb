c51530df3d779880e6aa7f6f785947068e01367f
Packaging Job <packaging@elastio.com>
2022-06-21 03:54:14 -0700

    feat/125-centos8-test-modify-origin-fails 7803

--- Casks/elastio-feat-125-centos8-test-modify-origin-fails.rb
+++ Casks/elastio-feat-125-centos8-test-modify-origin-fails.rb
@@ -5,9 +5,9 @@ cask "elastio-feat-125-centos8-test-modify-origin-fails" do
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "10c97b069148bdff0ba23ed6e6ffdc46fb63e2da7c1ef08a9aa6c0bd198d8338"
+    sha256 "555f86481e766179b7491dfce07a8e285da1e5651c3ac7b07848cf628ea88fbb"
   else
-    sha256 "860180cd4f82937dbf7136b2f24156bfd4ccd11d57647d6acb3f9f2306bacb90"
+    sha256 "2993cffb677a9384e6676218fb83cae4355d2adeb6d1f49208d9541cafd503e2"
   end
 
   url "https://s3.us-east-2.amazonaws.com/repo.assur.io/feat/125-centos8-test-modify-origin-fails/macos/#{arch}/elastio.zip"
