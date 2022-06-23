a4c09526f90ffc66dbd21a23e820cf262eb2e38f
Packaging Job <packaging@elastio.com>
2022-06-23 03:06:47 -0700

    trying 7932

--- Casks/elastio-trying.rb
+++ Casks/elastio-trying.rb
@@ -1,13 +1,13 @@
 cask "elastio-trying" do
 
-  version "0.18.4"
+  version "0.18.7"
 
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "66443edb4599378eff8eaeb795fb4dfe8cbcc5440c6fa271e6fee9c21faebcbd"
+    sha256 "a7dfa0340a2287f9126016fcb8b3434b0c61dbe2b4e48db8ba3704a0caafea2a"
   else
-    sha256 "4ccac427a75059b0a73e4de506648a2e4e6eedd29401cd638b55e1ea8fdd6fe4"
+    sha256 "150fdfd734e4aed5e99ec7adb848567dc94f3b631467ee536ca338b72e0bb09b"
   end
 
   url "https://s3.us-east-2.amazonaws.com/repo.assur.io/trying/macos/#{arch}/elastio.zip"
