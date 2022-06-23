f7bdfe8f985581be11d901a7dbc715ea22c5fdda
Packaging Job <packaging@elastio.com>
2022-06-23 05:35:55 -0700

    feat/mac-signing macOS-arm64-macarthur-osx-metal-2 7946

--- Casks/elastio-feat-mac-signing.rb
+++ Casks/elastio-feat-mac-signing.rb
@@ -5,9 +5,9 @@ cask "elastio-feat-mac-signing" do
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "0ec04acac44daec1e45cadd537ed8251eddb563be624779807e6718d26d6d590"
+    sha256 "ecb9d2b1933f67a048bca8868a82e83013d2f0dafcf980a93e75e65cefc94dbc"
   else
-    sha256 "3af2e2eec70e5e33cc13a117530607574e9c18ff2751b3a7fd34c4c62996d471"
+    sha256 "5165c20b9b9609e3d59b3302cc894876dbed4908a932ef9f1d7ad1700e3522df"
   end
 
   url "https://s3.us-east-2.amazonaws.com/repo.assur.io/feat/mac-signing/macos/#{arch}/elastio.zip"
