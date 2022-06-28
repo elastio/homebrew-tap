b50d2d525c92ad53ea058f6735a4107781cc2058
Packaging Job <packaging@elastio.com>
2022-06-28 03:15:47 -0700

    staging 8024

--- Casks/elastio-staging.rb
+++ Casks/elastio-staging.rb
@@ -1,13 +1,13 @@
 cask "elastio-staging" do
 
-  version "0.18.9"
+  version "0.18.10"
 
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "bad527e52fa6b5a6565a433ed1bc2471468d228e9852a83d24bb25d005359ec8"
+    sha256 "ce3f10da4e6fa95945fde15bd6ab4753d72ed984c6439ea002f927180f5f7932"
   else
-    sha256 "3a127ac58190cbdcbbc62d82b3486381a43f7c7ec358eb2ee3adec0cf4066d30"
+    sha256 "5895e98279a2e851b17b45d67bf129682ff28d39d22c38ac1a2079376c33c641"
   end
 
   url "https://s3.us-east-2.amazonaws.com/repo.assur.io/staging/macos/#{arch}/elastio.zip"
