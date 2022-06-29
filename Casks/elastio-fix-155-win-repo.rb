8a31a688362863f17b1a88abf30285e8936ecad2
Packaging Job <packaging@elastio.com>
2022-06-29 08:48:16 -0700

    fix/155-win-repo 8080

--- Casks/elastio-fix-155-win-repo.rb
+++ Casks/elastio-fix-155-win-repo.rb
@@ -5,9 +5,9 @@ cask "elastio-fix-155-win-repo" do
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "5e4f02354a9fdb7006afda6a89a6827e84b1acd34e3ae3dd4b08d67ac99d6734"
+    sha256 "c33ff7d01e459f5f9ebaf636314a017f9a2bba1cdd2a1cce68ced02d1cc3f3c5"
   else
-    sha256 "b7569e44522d3985a31a3dc1c7770023b8fdd0d384a51cfdc904d1a2ffdbbf36"
+    sha256 "e42e7fee6263657b65a0413e709a4e5986205380a8a400e93442cd169ef01c61"
   end
 
   url "https://s3.us-east-2.amazonaws.com/repo.assur.io/fix/155-win-repo/macos/#{arch}/elastio.zip"
