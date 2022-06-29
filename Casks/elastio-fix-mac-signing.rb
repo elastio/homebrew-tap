d392ed8a655d65bee5fe95309390c50b445abac1
Packaging Job <packaging@elastio.com>
2022-06-29 14:23:47 +0300

    fix/mac-signing vladas 8043

--- Casks/elastio-fix-mac-signing.rb
+++ Casks/elastio-fix-mac-signing.rb
@@ -5,9 +5,9 @@ cask "elastio-fix-mac-signing" do
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "34f543d924b4e8afcfe86475cfa977a11f20a8b03c76410a3cc4d83941b9df5c"
+    sha256 "ec4ffe90bf009f5bcaa2bc7d43a93663f7f4401811ff840e7837f6b2d76d0669"
   else
-    sha256 "d6c5867920e868c28b4a3f818515dc4ca3bd97791bd5271e79f7a95f1cabbbb1"
+    sha256 "83f18d6c1d3946c07d60d7a02e2df164c0064e837517739f6c62533d4925c228"
   end
 
   url "https://s3.us-east-2.amazonaws.com/repo.assur.io/fix/mac-signing/macos/#{arch}/elastio.zip"
