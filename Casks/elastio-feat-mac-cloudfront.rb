c8998dca402c8f7b5e8a0bc879adc752b5462182
Packaging Job <packaging@elastio.com>
2022-07-01 05:42:16 -0700

    feat/mac-cloudfront 8228

--- Casks/elastio-feat-mac-cloudfront.rb
+++ Casks/elastio-feat-mac-cloudfront.rb
@@ -5,12 +5,12 @@ cask "elastio-feat-mac-cloudfront" do
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "e11e8832186cbbd62bdec92bf520a858d3a9ae10ccfd385db05cea71ff2b3487"
+    sha256 "cea19006be49f1b88fb0c234befac2897b5e2072d5e4185b0921a4e28edac646"
   else
-    sha256 "518a625991f370bdf323305013d23a7e9d221e6ea6b2091a09cbd24872f382f0"
+    sha256 "1872e58b1a6ad2caa6e317902e5b5f312491acd7cc735ac04e5bd956f7f5c9ab"
   end
 
-  url "https://repo.assur.io/feat/mac-cloudfront/ver-82261656678355/macos/#{arch}/elastio.zip"
+  url "https://repo.assur.io/feat/mac-cloudfront/ver-82281656679241/macos/#{arch}/elastio.zip"
   name "Elastio"
   desc "The Data Protection Stage of Your Pipeline"
   homepage "https://elastio.com/"
