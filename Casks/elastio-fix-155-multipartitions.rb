125faed6ec4e642bff2c08e6d2b35b42be2f2d09
Packaging Job <packaging@elastio.com>
2022-08-20 03:30:22 -0700

    fix/155-multipartitions 8664

--- Casks/elastio-fix-155-multipartitions.rb
+++ Casks/elastio-fix-155-multipartitions.rb
@@ -1,16 +1,16 @@
 cask "elastio-fix-155-multipartitions" do
 
-  version "0.20.15"
+  version "0.20.18"
 
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "ad7ac03da042e10a8623956bf30be5b215f2ba7172a2aca0a4098a7c9c9156d2"
+    sha256 "e85df90e69f39fa7c55b143226d0e9b842ead77aeb9f78036a09fd7fdef8e83d"
   else
-    sha256 "a45add17e15a1ffa1339a2aa4d846238dc68447912ed5202d00bc8ba7622de65"
+    sha256 "ea9b286204d2a5cc0890d69edbbb462cbc393968fb03b53d9c96350061ce49ca"
   end
 
-  url "https://repo.assur.io/fix/155-multipartitions/ver-86161660738998/macos/#{arch}/elastio.zip"
+  url "https://repo.assur.io/fix/155-multipartitions/ver-86641660991206/macos/#{arch}/elastio.zip"
   name "Elastio"
   desc "The Data Protection Stage of Your Pipeline"
   homepage "https://elastio.com/"
