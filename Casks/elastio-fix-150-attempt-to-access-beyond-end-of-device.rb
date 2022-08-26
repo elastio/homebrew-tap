a5ecfe80e8fe1f09988fcf370d2f24f496c3cb18
Packaging Job <packaging@elastio.com>
2022-08-26 13:52:23 -0700

    fix/150-attempt-to-access-beyond-end-of-device 8709

--- Casks/elastio-fix-150-attempt-to-access-beyond-end-of-device.rb
+++ Casks/elastio-fix-150-attempt-to-access-beyond-end-of-device.rb
@@ -5,12 +5,12 @@ cask "elastio-fix-150-attempt-to-access-beyond-end-of-device" do
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "aef7dd8fd14484946361553ddec6b8d4aab5e0c0c2d29454bd92cba8908c85b4"
+    sha256 "7b51fbaa072b07f7261b9f50447dc82535016b49ffa2d969d4045e22abd0552b"
   else
-    sha256 "172a31865c985fde7c761c0e2029efadbda0517743e64f6e10033155b6e058a0"
+    sha256 "aed620bc77a2f0ebf856facfb5bd0c369a36937f4756c587571a6d1578bfff88"
   end
 
-  url "https://repo.assur.io/fix/150-attempt-to-access-beyond-end-of-device/ver-86711661218272/macos/#{arch}/elastio.zip"
+  url "https://repo.assur.io/fix/150-attempt-to-access-beyond-end-of-device/ver-87091661547049/macos/#{arch}/elastio.zip"
   name "Elastio"
   desc "The Data Protection Stage of Your Pipeline"
   homepage "https://elastio.com/"
