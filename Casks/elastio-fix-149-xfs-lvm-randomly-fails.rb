02f43ee3f4dcec8b9c5251eb95e12733b8a50334
Packaging Job <packaging@elastio.com>
2022-09-12 08:11:28 -0700

    fix/149-xfs-lvm-randomly-fails 8834

--- Casks/elastio-fix-149-xfs-lvm-randomly-fails.rb
+++ Casks/elastio-fix-149-xfs-lvm-randomly-fails.rb
@@ -5,12 +5,12 @@ cask "elastio-fix-149-xfs-lvm-randomly-fails" do
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "a582aa5b965618572346d6e9e1cfcd15204534d63989f5d7742505dc48c82856"
+    sha256 "148f2314943857414bff5cd7427c37c8f36660611169af059e90d6badf80666c"
   else
-    sha256 "d0fbca8ce4df05575f575ba984ecdc14683a31c60988c61706203996ea72baf2"
+    sha256 "e3b3b98bed9172d28b1ffbae43d2d5e226e6bdaf6d762840d190b4dca6db01a3"
   end
 
-  url "https://repo.assur.io/fix/149-xfs-lvm-randomly-fails/ver-88061662600243/macos/#{arch}/elastio.zip"
+  url "https://repo.assur.io/fix/149-xfs-lvm-randomly-fails/ver-88341662995371/macos/#{arch}/elastio.zip"
   name "Elastio"
   desc "The Data Protection Stage of Your Pipeline"
   homepage "https://elastio.com/"
