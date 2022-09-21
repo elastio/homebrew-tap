69dccc2e0a2cc2e0a5ab1907fcc4dfe680b062ee
Packaging Job <packaging@elastio.com>
2022-09-21 12:46:09 -0700

    fix/166-instability-issues-on-ubuntu2204 8919

--- Casks/elastio-fix-166-instability-issues-on-ubuntu2204.rb
+++ Casks/elastio-fix-166-instability-issues-on-ubuntu2204.rb
@@ -5,12 +5,12 @@ cask "elastio-fix-166-instability-issues-on-ubuntu2204" do
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "813899fc08c878f5b7979ee755cc5b70aff22de8c46f699e822f8223c6b0ae35"
+    sha256 "9dd61fe878ba8a85d61a4f474c9300fd53d0429b3f13767a6010e4cf36890e30"
   else
-    sha256 "9f76274b7a0cafc9b3c59a609027db0929f51a48c65a60a7374fa9ed59eb2f84"
+    sha256 "a01a9bfce35a0ba71a2dd1adbe198c3f538de46aad136413efdcef08cc53257f"
   end
 
-  url "https://repo.assur.io/fix/166-instability-issues-on-ubuntu2204/ver-89181663787244/macos/#{arch}/elastio.zip"
+  url "https://repo.assur.io/fix/166-instability-issues-on-ubuntu2204/ver-89191663789449/macos/#{arch}/elastio.zip"
   name "Elastio"
   desc "The Data Protection Stage of Your Pipeline"
   homepage "https://elastio.com/"
