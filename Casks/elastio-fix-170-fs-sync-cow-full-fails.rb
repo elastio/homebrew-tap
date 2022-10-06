bccaf62b7ae341d6d77ca6a629ce42dd942a326b
Packaging Job <packaging@elastio.com>
2022-10-05 18:47:50 -0700

    fix/170-fs-sync-cow-full-fails 9065

--- Casks/elastio-fix-170-fs-sync-cow-full-fails.rb
+++ Casks/elastio-fix-170-fs-sync-cow-full-fails.rb
@@ -5,12 +5,12 @@ cask "elastio-fix-170-fs-sync-cow-full-fails" do
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "796166983defc246398942c3d6e15ee310187f4e756ef9c672ae3081c6085044"
+    sha256 "79f0c4d36a7f3b1b42a028194d81158db417f8bdeea10cc4615c4c614dce2a89"
   else
-    sha256 "db19c5e3e46d67afec9f72e57c6dcb0c4f36368777b7f9bc0b62e42fd0f38471"
+    sha256 "ccf33cd4057977b4353284f8e4868b2e38b7d5fb7e9e24db0b37f414725df17a"
   end
 
-  url "https://repo.assur.io/fix/170-fs-sync-cow-full-fails/ver-90641665016921/macos/#{arch}/elastio.zip"
+  url "https://repo.assur.io/fix/170-fs-sync-cow-full-fails/ver-90651665020452/macos/#{arch}/elastio.zip"
   name "Elastio"
   desc "The Data Protection Stage of Your Pipeline"
   homepage "https://elastio.com/"
