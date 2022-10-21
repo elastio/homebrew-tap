c9da9cf817e81615b33d30f226d3bd6b6cac98eb
Packaging Job <packaging@elastio.com>
2022-10-20 20:59:46 -0700

    fix/176-module-does-not-compile-5.19.13 9158

--- Casks/elastio-fix-176-module-does-not-compile-5.19.13.rb
+++ Casks/elastio-fix-176-module-does-not-compile-5.19.13.rb
@@ -1,16 +1,16 @@
 cask "elastio-fix-176-module-does-not-compile-5.19.13" do
 
-  version "0.22.1"
+  version "0.22.2"
 
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "89b2f7fcf31875562c6a935ffb83543a11360638c8dfdad63f07eb9082584800"
+    sha256 "70368a4e7554bfcf386892d6f5fb5b7b8b42e83fdab34ad4730eb17b83b4fe06"
   else
-    sha256 "cda0b6725762efb50a01522eb7714909a0a1b59e9662abddad1ee67404109e71"
+    sha256 "5073e1437856fee8395f66f45ce6ed8b429c910f82e01c81bf7821cb2017aeca"
   end
 
-  url "https://repo.assur.io/fix/176-module-does-not-compile-5.19.13/ver-91541666271180/macos/#{arch}/elastio.zip"
+  url "https://repo.assur.io/fix/176-module-does-not-compile-5.19.13/ver-91581666324677/macos/#{arch}/elastio.zip"
   name "Elastio"
   desc "The Data Protection Stage of Your Pipeline"
   homepage "https://elastio.com/"
