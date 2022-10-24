e925b4f687e17a4b5a15beab87f2a1bc3f281a83
Packaging Job <packaging@elastio.com>
2022-10-24 16:59:53 -0700

    add/centos9 9185

--- Casks/elastio-add-centos9.rb
+++ Casks/elastio-add-centos9.rb
@@ -1,16 +1,16 @@
 cask "elastio-add-centos9" do
 
-  version "0.22.5"
+  version "0.22.6"
 
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "48a0cf6e9381239cf8cb722c279dc243ee397601e85d7bb145660107b6039d4c"
+    sha256 "483670be6b4f10d91d24c9cc11b1128f4b840683fdb10158753d25674c1c3b0a"
   else
-    sha256 "c9e3db1498044ae697f2804f18008456e3893a3d0db506639b9aa4f594281071"
+    sha256 "83e6c2b641189070d4c587b32e8df78b834202536991f68a58c0dd7ee4d0c8ef"
   end
 
-  url "https://repo.assur.io/add/centos9/ver-91751666617039/macos/#{arch}/elastio.zip"
+  url "https://repo.assur.io/add/centos9/ver-91851666655888/macos/#{arch}/elastio.zip"
   name "Elastio"
   desc "The Data Protection Stage of Your Pipeline"
   homepage "https://elastio.com/"
