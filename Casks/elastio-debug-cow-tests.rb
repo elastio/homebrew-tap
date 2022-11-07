423e345bc61be5feb2161da61261f3b71794a78b
Packaging Job <packaging@elastio.com>
2022-11-07 09:40:08 -0800

    debug/cow-tests 9301

--- Casks/elastio-debug-cow-tests.rb
+++ Casks/elastio-debug-cow-tests.rb
@@ -1,16 +1,16 @@
 cask "elastio-debug-cow-tests" do
 
-  version "0.22.14"
+  version "0.22.17"
 
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "0a67107dd5ad2c5b3b6a72c6d34a554a9230f6b13add8d74a6409f9dd6f185ad"
+    sha256 "a2c572cd9dff165df11875c8d1f346f61d77460cf89dea273d9fed7d6de34864"
   else
-    sha256 "b4f77e5545db778d63193d4cb3b2313e0b5b71579faaf51987e76038dd8bc15c"
+    sha256 "83f61b9aa8d38cab0d5ea62b974ca28473da352d5eb5c9835e2ea545ad74dcda"
   end
 
-  url "https://repo.assur.io/debug/cow-tests/ver-92811667518197/macos/#{arch}/elastio.zip"
+  url "https://repo.assur.io/debug/cow-tests/ver-93011667842696/macos/#{arch}/elastio.zip"
   name "Elastio"
   desc "The Data Protection Stage of Your Pipeline"
   homepage "https://elastio.com/"
