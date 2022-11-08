57a0be2a997a59021aa2a99d486cf9288110d25c
Packaging Job <packaging@elastio.com>
2022-11-08 00:13:19 -0800

    add-driver-stress-test 9309

--- Casks/elastio-add-driver-stress-test.rb
+++ Casks/elastio-add-driver-stress-test.rb
@@ -1,16 +1,16 @@
 cask "elastio-add-driver-stress-test" do
 
-  version "0.21.27"
+  version "0.22.18"
 
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "9c06b711719c8a0db42e4cf8b00bb1eebfdbaeddc37c832fc3d6b9e843f293d9"
+    sha256 "7b296cc0def65a5ced2043006acae45b147134ada4c0ef936b3fdb44d46ff16c"
   else
-    sha256 "ada9941d3d00b2277cecfa9f8a9d234f7de48b73ca064562f28da4fa1c43f18f"
+    sha256 "49cb7dffa4eef1b22b788b98b96e4a3c323ff20e47d3b1db25d5c68022212187"
   end
 
-  url "https://repo.assur.io/add-driver-stress-test/ver-91421666107603/macos/#{arch}/elastio.zip"
+  url "https://repo.assur.io/add-driver-stress-test/ver-93091667895092/macos/#{arch}/elastio.zip"
   name "Elastio"
   desc "The Data Protection Stage of Your Pipeline"
   homepage "https://elastio.com/"
