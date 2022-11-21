7c91d14735ed9f5056e5618006cd6a506a9ba17f
Packaging Job <packaging@elastio.com>
2022-11-21 06:00:36 -0800

    adds-linux-stress-test 9408

--- Casks/elastio-adds-linux-stress-test.rb
+++ Casks/elastio-adds-linux-stress-test.rb
@@ -1,16 +1,16 @@
 cask "elastio-adds-linux-stress-test" do
 
-  version "0.22.24"
+  version "0.22.26"
 
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "9a6463e524d8b115af818148504c934744e5744f388691aa4c4a69fbaf1f882c"
+    sha256 "f8652d334a60c813740566ee5922163170dcce390897c669559af2b4bcaf78ef"
   else
-    sha256 "08b0f71f56b27ce9f5d78149a28b11041cc887d258063f598520857600bf558e"
+    sha256 "330fd17ae719cf5468aa1dc4097fc6108d4b4800a266987f28a84757f091aa38"
   end
 
-  url "https://repo.assur.io/adds-linux-stress-test/ver-93931668784710/macos/#{arch}/elastio.zip"
+  url "https://repo.assur.io/adds-linux-stress-test/ver-94081669039117/macos/#{arch}/elastio.zip"
   name "Elastio"
   desc "The Data Protection Stage of Your Pipeline"
   homepage "https://elastio.com/"
