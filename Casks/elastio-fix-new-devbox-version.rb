9d2b54a9d843cf580d362d5f15f86c4b0f72417d
Packaging Job <packaging@elastio.com>
2022-11-22 00:43:45 -0800

    fix-new-devbox-version 9417

--- Casks/elastio-fix-new-devbox-version.rb
+++ Casks/elastio-fix-new-devbox-version.rb
@@ -1,16 +1,16 @@
 cask "elastio-fix-new-devbox-version" do
 
-  version "0.23.0"
+  version "0.23.1"
 
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "a976003ae3cb4555cf47e785aedfdce64fbafcde1965bf68ea6bed31ca28fe0c"
+    sha256 "aff15f83b0855e1f17659095f01d12b124660bbc02b64384d22fd6ab4bbee233"
   else
-    sha256 "6edaab13a8634a534209ac1a29321dc39ba74e6969cc427fb4ae2ce3034c335f"
+    sha256 "4944f0741e9bbb101f8582be012ff9650a45f94abe01671fc5bb981dd6c01706"
   end
 
-  url "https://repo.assur.io/fix-new-devbox-version/ver-94111669043627/macos/#{arch}/elastio.zip"
+  url "https://repo.assur.io/fix-new-devbox-version/ver-94171669106501/macos/#{arch}/elastio.zip"
   name "Elastio"
   desc "The Data Protection Stage of Your Pipeline"
   homepage "https://elastio.com/"
