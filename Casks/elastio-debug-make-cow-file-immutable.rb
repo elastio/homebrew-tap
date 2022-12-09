7c4a11f136f9ea4e1fa77b1e36f6ffa8fdb99b5b
Packaging Job <packaging@elastio.com>
2022-12-09 07:13:45 -0800

    debug/make-cow-file-immutable 9604

--- Casks/elastio-debug-make-cow-file-immutable.rb
+++ Casks/elastio-debug-make-cow-file-immutable.rb
@@ -1,16 +1,16 @@
 cask "elastio-debug-make-cow-file-immutable" do
 
-  version "0.23.9"
+  version "0.23.13"
 
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "ba863ef89ea236025e2bcdb8a9f7970d19f0d14f9bd4cf1164d6538300e670e0"
+    sha256 "cb4d528d66e4ad6406cdc43ba2b1c35d44736bafb31b38231d5b6264f654fe5e"
   else
-    sha256 "a9c6b12733f68bd4fdde55c5137e8e8fa9fc86e64df72efcfd33ce32585769b8"
+    sha256 "023e32b3b5d81424d9ea00c36653b8d22fbe4b781e36deeb3fc658c459bf283d"
   end
 
-  url "https://repo.assur.io/debug/make-cow-file-immutable/ver-95541670269002/macos/#{arch}/elastio.zip"
+  url "https://repo.assur.io/debug/make-cow-file-immutable/ver-96041670598707/macos/#{arch}/elastio.zip"
   name "Elastio"
   desc "The Data Protection Stage of Your Pipeline"
   homepage "https://elastio.com/"
