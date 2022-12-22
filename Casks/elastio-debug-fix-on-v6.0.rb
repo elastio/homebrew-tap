c60d1f0b7ceeb2e1ee4891037da87fac1c0c3356
Packaging Job <packaging@elastio.com>
2022-12-22 04:46:41 -0800

    debug/fix-on-v6.0 9702

--- Casks/elastio-debug-fix-on-v6.0.rb
+++ Casks/elastio-debug-fix-on-v6.0.rb
@@ -1,16 +1,16 @@
 cask "elastio-debug-fix-on-v6.0" do
 
-  version "0.23.20"
+  version "0.23.22"
 
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "ff0b66b31105733be22f4df0d30cfa11e3278120b43515f750c7ec924d9816cd"
+    sha256 "a25d34037ca24810bdd9c6ee2ae63efc580188560a15b7b083f2ca8f83d11f81"
   else
-    sha256 "c90cbd430209b170aac217215e954b7323e83b56828fec597df4f5d6dd554bcb"
+    sha256 "915d5e37c7f59b883ea0d46d5d1cc40bbec6a3ec39012cb59432c7e69266c8ee"
   end
 
-  url "https://repo.assur.io/debug/fix-on-v6.0/ver-96911671582996/macos/#{arch}/elastio.zip"
+  url "https://repo.assur.io/debug/fix-on-v6.0/ver-97021671713081/macos/#{arch}/elastio.zip"
   name "Elastio"
   desc "The Data Protection Stage of Your Pipeline"
   homepage "https://elastio.com/"
