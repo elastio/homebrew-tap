466c649b341c8cc71c5325e73d4d325438cdf36d
Packaging Job <packaging@elastio.com>
2023-01-11 03:54:19 -0800

    fix-win-smoke 9829

--- Casks/elastio-fix-win-smoke.rb
+++ Casks/elastio-fix-win-smoke.rb
@@ -1,16 +1,16 @@
 cask "elastio-fix-win-smoke" do
 
-  version "0.20.18"
+  version "0.23.33"
 
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "0a26a13789e85b2aeff760011cef5e95acad132e06419abafeb48026c8cdd31e"
+    sha256 "4a62382d546c9d1ccee6cd3420bcff2be6e9ba5fde68d1978587527fd86e7c8c"
   else
-    sha256 "c8237548f2eff11f118c94c42306015beb08b5f87ab2d0aac3c6d93563a4d6b4"
+    sha256 "b7421952d7b3b4c5d05d78de04750193a2512a03d4c93f9ffc0d9cd2cfb208dd"
   end
 
-  url "https://repo.assur.io/fix-win-smoke/ver-87031661524999/macos/#{arch}/elastio.zip"
+  url "https://repo.assur.io/fix-win-smoke/ver-98291673437936/macos/#{arch}/elastio.zip"
   name "Elastio"
   desc "The Data Protection Stage of Your Pipeline"
   homepage "https://elastio.com/"
