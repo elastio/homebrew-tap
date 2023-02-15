831ad6743f3f93935e9df705e464722d75a32b21
Packaging Job <packaging@elastio.com>
2023-02-15 12:06:47 -0800

    feat/6156-infra-migrate-to-oidc-roles-for-elastio-and-packaging-repos 10148

--- Casks/elastio-feat-6156-infra-migrate-to-oidc-roles-for-elastio-and-packaging-repos.rb
+++ Casks/elastio-feat-6156-infra-migrate-to-oidc-roles-for-elastio-and-packaging-repos.rb
@@ -5,12 +5,12 @@ cask "elastio-feat-6156-infra-migrate-to-oidc-roles-for-elastio-and-packaging-re
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "0c5c1368d21a51ca8d52fbe722d92c7f04699316a4330b87689bc5a4f61066f1"
+    sha256 "a01282e20c3be2dbf2890a1a13321d1eb811125e9442d85a2f6168018f8c77eb"
   else
-    sha256 "e715a6f49cf16457386183949c93ddb96a8cc3b093207a2cd9f9899a7b22fab8"
+    sha256 "ae0e08680322e9f2e346bdadd6d185609c837c5e68acd8d41de1860ce12e2dd7"
   end
 
-  url "https://repo.assur.io/feat/6156-infra-migrate-to-oidc-roles-for-elastio-and-packaging-repos/ver-101461676491266/macos/#{arch}/elastio.zip"
+  url "https://repo.assur.io/feat/6156-infra-migrate-to-oidc-roles-for-elastio-and-packaging-repos/ver-101481676491485/macos/#{arch}/elastio.zip"
   name "Elastio"
   desc "The Data Protection Stage of Your Pipeline"
   homepage "https://elastio.com/"
