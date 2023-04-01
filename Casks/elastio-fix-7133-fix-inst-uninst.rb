376de38fb869bdc042ab898291a23b4c42c0b193
Packaging Job <packaging@elastio.com>
2023-04-01 08:39:32 -0700

    fix/7133-fix-inst-uninst 10498

--- Casks/elastio-fix-7133-fix-inst-uninst.rb
+++ Casks/elastio-fix-7133-fix-inst-uninst.rb
@@ -1,16 +1,16 @@
 cask "elastio-fix-7133-fix-inst-uninst" do
 
-  version "0.24.29"
+  version "0.24.30"
 
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "55a07ca893bcdc6ededad3c792115f77de59d206c8739a77a736c798ecade153"
+    sha256 "c7a150aab5098a4abc3f432f97caf1b392d4746eecc7ef4d0fb5508aa96f22e2"
   else
-    sha256 "c6934f1403c7169d757e1efe4b7ebc9cb9ec5c991503c9286244b64ea8456c75"
+    sha256 "1c2d340bb2cd5d4c0762fc23302fedcacfb1ad491dde520f081652a0502842af"
   end
 
-  url "https://repo.assur.io/fix/7133-fix-inst-uninst/ver-104881680279439/macos/#{arch}/elastio.zip"
+  url "https://repo.assur.io/fix/7133-fix-inst-uninst/ver-104981680363439/macos/#{arch}/elastio.zip"
   name "Elastio"
   desc "The Data Protection Stage of Your Pipeline"
   homepage "https://elastio.com/"
