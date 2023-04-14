716089e482444930068bf1a95e486665a5b11a10
Packaging Job <packaging@elastio.com>
2023-04-14 01:34:36 -0700

    debug/set-build-num-990 10611

--- Casks/elastio-debug-set-build-num-990.rb
+++ Casks/elastio-debug-set-build-num-990.rb
@@ -5,12 +5,12 @@ cask "elastio-debug-set-build-num-990" do
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "d563199fd52293bd03a5cc1597c9ac925574962af8f2b7001b5af8518c1a1c72"
+    sha256 "8a299d11e216ed10bdf881d826836b7a9b563782fa089c7e1057c29111aa4b48"
   else
-    sha256 "6225e54da5cf8e4443494ca79a6523f5dcf4e85f23fc3a409e323291be9f0ef0"
+    sha256 "45dba5ceb2aa8d4d8df1dbbfdef19e12b6bcf9d7fb431bea8dce0e16a272ecfe"
   end
 
-  url "https://repo.assur.io/debug/set-build-num-990/ver-106091681460344/macos/#{arch}/elastio.zip"
+  url "https://repo.assur.io/debug/set-build-num-990/ver-106111681461158/macos/#{arch}/elastio.zip"
   name "Elastio"
   desc "The Data Protection Stage of Your Pipeline"
   homepage "https://elastio.com/"
