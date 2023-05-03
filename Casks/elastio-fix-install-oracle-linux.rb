befbb2d221eb9bd5f1ad510b147a22b572b73edc
Packaging Job <packaging@elastio.com>
2023-05-03 09:28:47 -0700

    fix/install-oracle-linux 10732

--- Casks/elastio-fix-install-oracle-linux.rb
+++ Casks/elastio-fix-install-oracle-linux.rb
@@ -5,12 +5,12 @@ cask "elastio-fix-install-oracle-linux" do
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "cb9f5c2fb88c7e01b27dc967d28c6fda6d963bf8f611560a7ec9136fa3ca5aca"
+    sha256 "13307c419be04f53a8fb39eaac1110baaec81aea17f0c7e69537813f05c72641"
   else
-    sha256 "7f25ba78862519c60255456b952ae7e5183f13b132de80d35d7625a6f20c2631"
+    sha256 "39a719d17673967fa35dce2a0d15f8b46b4a966b63fc19be231882a7d92744c3"
   end
 
-  url "https://repo.assur.io/fix/install-oracle-linux/ver-107301683130722/macos/#{arch}/elastio.zip"
+  url "https://repo.assur.io/fix/install-oracle-linux/ver-107321683131197/macos/#{arch}/elastio.zip"
   name "Elastio"
   desc "The Data Protection Stage of Your Pipeline"
   homepage "https://elastio.com/"
