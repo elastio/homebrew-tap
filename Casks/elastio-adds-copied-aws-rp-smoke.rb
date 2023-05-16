ac78eb1d99696cd7d209cd86bcd4dc5f6942c9cc
Packaging Job <packaging@elastio.com>
2023-05-16 13:03:23 -0700

    adds-copied-aws-rp-smoke 10856

--- Casks/elastio-adds-copied-aws-rp-smoke.rb
+++ Casks/elastio-adds-copied-aws-rp-smoke.rb
@@ -5,12 +5,12 @@ cask "elastio-adds-copied-aws-rp-smoke" do
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "7cee68b58f66d8c0f656378de9c2256be95443aee6809345dcb6da37df6eb2d1"
+    sha256 "728a2a7b9bd84345b32b2e6acc8d8569658d6c5ac88b962c257a48c9d9b301a5"
   else
-    sha256 "7698e0e065f075d97a24e16a4d15eadc1e18cf97bdb821ad02d392777ec8fff2"
+    sha256 "df851c6fad2528070f620b02a7dc221806330c1e7e2e6e1649d4ef0a0acd5ffd"
   end
 
-  url "https://repo.assur.io/adds-copied-aws-rp-smoke/ver-108551684266918/macos/#{arch}/elastio.zip"
+  url "https://repo.assur.io/adds-copied-aws-rp-smoke/ver-108561684267281/macos/#{arch}/elastio.zip"
   name "Elastio"
   desc "The Data Protection Stage of Your Pipeline"
   homepage "https://elastio.com/"
