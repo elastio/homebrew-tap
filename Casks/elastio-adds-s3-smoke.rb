edf2c50abdf6bc83f887fe9167e81401ded0ae02
Packaging Job <packaging@elastio.com>
2023-07-06 02:17:54 -0700

    adds-s3-smoke 11211

--- Casks/elastio-adds-s3-smoke.rb
+++ Casks/elastio-adds-s3-smoke.rb
@@ -5,12 +5,12 @@ cask "elastio-adds-s3-smoke" do
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "5e9b13dd6206feaeb3da987f340eb82dd51d3f4b55a8cc885c9b824f54557a13"
+    sha256 "587471e2429987274156df294fd7438d8f3f2f854e25a994ebbd58ac26e9859f"
   else
-    sha256 "4242e263242ed18c57931efc5c13c92aa396039673cd33077bddac1c45a5e181"
+    sha256 "96e311f16fa37227b4f9ec0f6ac1fafa35fc256a6b21c83ebab7a39bc90f32b6"
   end
 
-  url "https://repo.assur.io/adds-s3-smoke/ver-112101688633098/macos/#{arch}/elastio.zip"
+  url "https://repo.assur.io/adds-s3-smoke/ver-112111688634942/macos/#{arch}/elastio.zip"
   name "Elastio"
   desc "The Data Protection Stage of Your Pipeline"
   homepage "https://elastio.com/"
