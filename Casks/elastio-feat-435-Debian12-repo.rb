eed1aff315f0d46eab8aa107d147d927495c1cf1
Packaging Job <packaging@elastio.com>
2023-07-07 01:33:54 -0700

    feat/435-Debian12-repo 11225

--- Casks/elastio-feat-435-Debian12-repo.rb
+++ Casks/elastio-feat-435-Debian12-repo.rb
@@ -1,16 +1,16 @@
 cask "elastio-feat-435-Debian12-repo" do
 
-  version "0.27.10"
+  version "0.27.11"
 
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "0e260fd572b442e324b8d6da7745c0a4a7b27875c4ebf942af19fc181b3da27e"
+    sha256 "0169d4214a04ee7db19e7d0bdbbcf32e6c286c96f1f4f71f6790983b5521b62d"
   else
-    sha256 "ef4950c3c8decce5096c31f78f36edabc8c4a307b2c1211bf0297bc7136ccfab"
+    sha256 "7a82a63f30d03114d9554791cde56b7ecff0e0cc38b9441e4720c7a7324790ad"
   end
 
-  url "https://repo.assur.io/feat/435-Debian12-repo/ver-112161688658468/macos/#{arch}/elastio.zip"
+  url "https://repo.assur.io/feat/435-Debian12-repo/ver-112251688718710/macos/#{arch}/elastio.zip"
   name "Elastio"
   desc "The Data Protection Stage of Your Pipeline"
   homepage "https://elastio.com/"
