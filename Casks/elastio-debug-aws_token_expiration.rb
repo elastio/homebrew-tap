52cae0098e39ee9c3d9745ad8a0e17d3df93d926
Packaging Job <packaging@elastio.com>
2023-07-21 03:55:29 -0700

    debug/aws_token_expiration 11340

--- Casks/elastio-debug-aws_token_expiration.rb
+++ Casks/elastio-debug-aws_token_expiration.rb
@@ -5,12 +5,12 @@ cask "elastio-debug-aws_token_expiration" do
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "b0248a8b9167d75dee951c0811dc99873a762aae14c32f3ab26e85ef528e68cd"
+    sha256 "94930a26e2889213083765c8a29b15c5bf4189bc5c6ae9feb275e2382024c2aa"
   else
-    sha256 "ce62648e5d58984757c97e55f86878d4311ca3d190eecdbf499b6ec7bd6a3d5c"
+    sha256 "42252c41b98816c17211baf02407df47b5edb5d9cffc827423ffd2259e3d2e50"
   end
 
-  url "https://repo.assur.io/debug/aws_token_expiration/ver-113281689807779/macos/#{arch}/elastio.zip"
+  url "https://repo.assur.io/debug/aws_token_expiration/ver-113401689936796/macos/#{arch}/elastio.zip"
   name "Elastio"
   desc "The Data Protection Stage of Your Pipeline"
   homepage "https://elastio.com/"
