e32aac0798ab5e2e5fa514ac0a8e628dfabeebc7
Packaging Job <packaging@elastio.com>
2023-09-29 00:08:06 -0700

    fix-smoke-restore 11864

--- Casks/elastio-fix-smoke-restore.rb
+++ Casks/elastio-fix-smoke-restore.rb
@@ -5,12 +5,12 @@ cask "elastio-fix-smoke-restore" do
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "afcd6fcdbffd8ccfb90a72204659bd414a878cf3fe36df3c2298bb339cbc560a"
+    sha256 "7bb007b1e0dd991e878b33f0598197c524500acec38701962b9227d9fbee2ad5"
   else
-    sha256 "8c3bec9bd77812885ac41df8446211009f9f48ce07c622c62226397b3ca83a1f"
+    sha256 "da4fe821582da79a8aad93ff532264340db9e0e9b876ff4b09407748a82210f9"
   end
 
-  url "https://repo.assur.io/fix-smoke-restore/ver-118631695970633/macos/#{arch}/elastio.zip"
+  url "https://repo.assur.io/fix-smoke-restore/ver-118641695971161/macos/#{arch}/elastio.zip"
   name "Elastio"
   desc "The Data Protection Stage of Your Pipeline"
   homepage "https://elastio.com/"
