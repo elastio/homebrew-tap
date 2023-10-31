3dc787dbf9ab748ad115b7180d2e97e18b61992b
Packaging Job <packaging@elastio.com>
2023-10-30 18:33:27 -0700

    staging 12157

--- Casks/elastio-staging.rb
+++ Casks/elastio-staging.rb
@@ -5,12 +5,12 @@ cask "elastio-staging" do
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "db6ca65b55e1c75473457fd6e5bcbf14cc45b8771871388657f181a22cd8808a"
+    sha256 "de2974458c338c2552f37026e4cd69651eb9b63f18a33e4c8f19cbc33ee5b5d1"
   else
-    sha256 "92b375d647a49c3ffc71e076524a7525eb96aafd219a8072da4abd1082b897ad"
+    sha256 "f2ba65514ed867e39c66e33b43e50c7605d0b963af0f5980bd029a82fd5fc017"
   end
 
-  url "https://repo.assur.io/staging/ver-121561698710362/macos/#{arch}/elastio.zip"
+  url "https://repo.assur.io/staging/ver-121571698715868/macos/#{arch}/elastio.zip"
   name "Elastio"
   desc "The Data Protection Stage of Your Pipeline"
   homepage "https://elastio.com/"
