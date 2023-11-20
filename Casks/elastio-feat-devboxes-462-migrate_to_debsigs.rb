1998b4ee587992a8473ce1fa1c43b62b4dd74830
Packaging Job <packaging@elastio.com>
2023-11-20 13:52:25 -0800

    feat/devboxes-462/migrate_to_debsigs 12355

--- Casks/elastio-feat-devboxes-462-migrate_to_debsigs.rb
+++ Casks/elastio-feat-devboxes-462-migrate_to_debsigs.rb
@@ -5,12 +5,12 @@ cask "elastio-feat-devboxes-462-migrate_to_debsigs" do
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "035ac5a614d110b1b24db11c7fef0696e313b7f811d33b7ee856ada18f7c08c3"
+    sha256 "69be53dbdb361653c8ae24ba620267abe40aa76887a341556437a1a62b23f2d2"
   else
-    sha256 "d44eaac84b5604ac2cfef06634cdb0d7c73c5f6cbf148915711074b9d8d137af"
+    sha256 "4b9748a56e75af4215403537a8afe5afd011882839b205bff8fa165f1428bbcc"
   end
 
-  url "https://repo.assur.io/feat/devboxes-462/migrate_to_debsigs/ver-123541700514189/macos/#{arch}/elastio.zip"
+  url "https://repo.assur.io/feat/devboxes-462/migrate_to_debsigs/ver-123551700517006/macos/#{arch}/elastio.zip"
   name "Elastio"
   desc "The Data Protection Stage of Your Pipeline"
   homepage "https://elastio.com/"
