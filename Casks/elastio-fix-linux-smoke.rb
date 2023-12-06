b90b5ae7bdad3ae9695b7f3c5377db18c6a4944f
Packaging Job <packaging@elastio.com>
2023-12-06 01:10:19 -0800

    fix-linux-smoke 12509

--- Casks/elastio-fix-linux-smoke.rb
+++ Casks/elastio-fix-linux-smoke.rb
@@ -1,16 +1,16 @@
 cask "elastio-fix-linux-smoke" do
 
-  version "0.24.5"
+  version "0.29.19"
 
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "8df33c904460fac257fa9ee6b3eade93ddd5adaa009124be56c128ae37f83abd"
+    sha256 "00ac36e9f7e6adc8819d66b988247b68eb93cb55b1594ee5e82beb25c281b226"
   else
-    sha256 "43f332e88ec9071e7a579fc7d00359a2260b8f86e3b9b55f76be0514bd380ca6"
+    sha256 "16c0d702097a10dca81cf1043d6b38f928806bcb10e0fb54d52b61b66bec18d9"
   end
 
-  url "https://repo.assur.io/fix-linux-smoke/ver-101611676550619/macos/#{arch}/elastio.zip"
+  url "https://repo.assur.io/fix-linux-smoke/ver-125091701853695/macos/#{arch}/elastio.zip"
   name "Elastio"
   desc "The Data Protection Stage of Your Pipeline"
   homepage "https://elastio.com/"
