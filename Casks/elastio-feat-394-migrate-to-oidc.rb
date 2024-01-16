cf1589f457c3d0a52a1f6484f970d67b1f28d9d1
Packaging Job <packaging@elastio.com>
2024-01-16 09:25:26 -0800

    feat/394/migrate-to-oidc 12759

--- Casks/elastio-feat-394-migrate-to-oidc.rb
+++ Casks/elastio-feat-394-migrate-to-oidc.rb
@@ -1,16 +1,16 @@
 cask "elastio-feat-394-migrate-to-oidc" do
 
-  version "0.29.32"
+  version "0.29.43"
 
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "c935cab5ecbd5222a030d67804b408637227c42b13d457ce09e014cf9d0b5eb8"
+    sha256 "fdf1d783a403b542dfb2041cf938a1343180d5bf7f5f1f717028d8239ad47c98"
   else
-    sha256 "d2cfc5c3871f477c41fd176f30ee86994cbec4611f9ecdf0ac3db8bcbbc97579"
+    sha256 "788dddbe920574924bcb85011199f0486b6d5f85f36d2769b4d1dda91a486cf8"
   end
 
-  url "https://repo.assur.io/feat/394/migrate-to-oidc/ver-126431704184155/macos/#{arch}/elastio.zip"
+  url "https://repo.assur.io/feat/394/migrate-to-oidc/ver-127591705425785/macos/#{arch}/elastio.zip"
   name "Elastio"
   desc "The Data Protection Stage of Your Pipeline"
   homepage "https://elastio.com/"
