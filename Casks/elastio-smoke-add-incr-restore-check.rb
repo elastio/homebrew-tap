f56af20bb24962f196595cebe853fe535f0b2791
Packaging Job <packaging@elastio.com>
2024-02-02 05:45:32 -0800

    smoke-add-incr-restore-check 12992

--- Casks/elastio-smoke-add-incr-restore-check.rb
+++ Casks/elastio-smoke-add-incr-restore-check.rb
@@ -5,12 +5,12 @@ cask "elastio-smoke-add-incr-restore-check" do
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "098e8db3abeb4aed4660bcc13a389229d851ed2a3f7c2975a2f77315c1a64883"
+    sha256 "d310ed0b68c70f2d3e1ed675d7c539cac828eeeba6ed1d3d148304d5b6684cbb"
   else
-    sha256 "1efed6fe748f8c37abd1349958803c9e8f3b3321ebede2a1a863b7375906b71a"
+    sha256 "331fcd832d1131492302df991b8b21ee50e71341f71c9245e09b275ceedd6078"
   end
 
-  url "https://repo.assur.io/smoke-add-incr-restore-check/ver-129901706880985/macos/#{arch}/elastio.zip"
+  url "https://repo.assur.io/smoke-add-incr-restore-check/ver-129921706881416/macos/#{arch}/elastio.zip"
   name "Elastio"
   desc "The Data Protection Stage of Your Pipeline"
   homepage "https://elastio.com/"
