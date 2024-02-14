6e04f6b05a54e918f756647dcb513d5532fbbbb4
Packaging Job <packaging@elastio.com>
2024-02-14 14:57:57 -0500

    feat/smoke_optimize 13117

--- Casks/elastio-feat-smoke_optimize.rb
+++ Casks/elastio-feat-smoke_optimize.rb
@@ -5,12 +5,12 @@ cask "elastio-feat-smoke_optimize" do
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "829065c0c170f3e99a0f52ff3e03fa50372d88f979c92081361dd907c065772a"
+    sha256 "4f2c25e371eec1591e78ee9ebc4f057eb538c245041848d4c8e5f9dad84bc1de"
   else
-    sha256 "082ce7b1a60ed21f1939c86326d231e7e001705462ae2886cdda50140ee4da6f"
+    sha256 "9835eee638656283558dfe0086e94b417f32f27e413ade7a91ac982e8f470917"
   end
 
-  url "https://repo.assur.io/feat/smoke_optimize/ver-131151707940383/macos/#{arch}/elastio.zip"
+  url "https://repo.assur.io/feat/smoke_optimize/ver-131171707940553/macos/#{arch}/elastio.zip"
   name "Elastio"
   desc "The Data Protection Stage of Your Pipeline"
   homepage "https://elastio.com/"
