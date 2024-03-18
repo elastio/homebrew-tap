da467847a6b0b44e3c4cb4c15f6be2053a027e58
Packaging Job <packaging@elastio.com>
2024-03-18 05:22:40 -0700

    feat/8589/add_upload_to_repos_elastio 13483

--- Casks/elastio-feat-8589-add_upload_to_repos_elastio.rb
+++ Casks/elastio-feat-8589-add_upload_to_repos_elastio.rb
@@ -1,16 +1,16 @@
 cask "elastio-feat-8589-add_upload_to_repos_elastio" do
 
-  version "0.30.12"
+  version "0.30.13"
 
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "8dcce779931621f03632773aae6e808273f025ce49573e0d198e863fe84eabaa"
+    sha256 "83888334dc19e9da32a1f1d8ef9749252a7d089844dc3944f8f5fe6af8583cfd"
   else
-    sha256 "6303f122b3e01a2afa533c1a6d93f3febba1c6ca2404c262fcea091ef03ee7cf"
+    sha256 "a8cf8e0e4c06066cf622373d4101710e3456a51401eee6e388c47441af6cea88"
   end
 
-  url "https://repo.elastio.us/feat/8589/add_upload_to_repos_elastio/ver-134731710503772/macos/#{arch}/elastio.zip"
+  url "https://repo.elastio.us/feat/8589/add_upload_to_repos_elastio/ver-134831710764425/macos/#{arch}/elastio.zip"
   name "Elastio"
   desc "The Data Protection Stage of Your Pipeline"
   homepage "https://elastio.com/"
