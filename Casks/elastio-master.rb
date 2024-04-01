afebe34f4ab007a3d102b994912e04e4093765e7
Packaging Job <packaging@elastio.com>
2024-04-01 03:44:23 -0700

    master 13588

--- Casks/elastio-master.rb
+++ Casks/elastio-master.rb
@@ -1,16 +1,16 @@
 cask "elastio-master" do
 
-  version "0.30.22"
+  version "0.30.23"
 
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "62821cc92c20b21a7fdbc6f307e177488a792e91e088ce696cb96aee6f8446d6"
+    sha256 "df5e896b7616865330de4dec65e1e439c6461010884c783eb020d398990a7526"
   else
-    sha256 "ccbd33eb532ef327698dfd82fc989e70550fa3ed41ccaeffc6fc82741a8c86d5"
+    sha256 "3d015644960a0c71f106197e762b03ef40c41888036d07fe22031f0e131d548b"
   end
 
-  url "https://repo.elastio.us/master/ver-135861711748987/macos/#{arch}/elastio.zip"
+  url "https://repo.elastio.us/master/ver-135881711968140/macos/#{arch}/elastio.zip"
   name "Elastio"
   desc "The Data Protection Stage of Your Pipeline"
   homepage "https://elastio.com/"
