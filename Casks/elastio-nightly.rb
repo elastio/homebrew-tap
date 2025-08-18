a3dd90f528b7e27a52a661702d8a61cfb9f2422f
Packaging Job <packaging@elastio.com>
2025-08-18 00:12:11 -0400

    nightly 16136

--- Casks/elastio-nightly.rb
+++ Casks/elastio-nightly.rb
@@ -1,16 +1,16 @@
 cask "elastio-nightly" do
 
-  version "0.38.11"
+  version "0.38.12"
 
   arch = Hardware::CPU.intel? ? "x86_64" : "aarch64"
 
   if Hardware::CPU.intel?
-    sha256 "b1c432c7dfd88fea8409f0158bd2f80a537d2c1c61063d79dd5b756902418dd5"
+    sha256 "433684120841f0b4bb07eb567bfcd19093ef6696e7b3b953ee84a6d1d2cd98e5"
   else
-    sha256 "d29d3a3484da38a9b5b1a550e765f3bb9c11c76217cecd8abcf2bb6e51cc6274"
+    sha256 "0f0000b7b251bbca56ede3daa34152ed21dc5a6035518fe8af4338d77c553614"
   end
 
-  url "https://repo.elastio.com/nightly/ver-161321755327251/macos/#{arch}/elastio.zip"
+  url "https://repo.elastio.com/nightly/ver-161361755490219/macos/#{arch}/elastio.zip"
   name "Elastio"
   desc "The Data Protection Stage of Your Pipeline"
   homepage "https://elastio.com/"
